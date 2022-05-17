df28446d1c9adb6af88632dac1c62dce5b379d89
dppyfrsh <tux@server.example.com>
2022-05-18 02:03:02 +0800

    Break Test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("breakthis");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
